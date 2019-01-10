57fdfd2f04109346ce4930ba5a293301e6cbb6d8
bocheng0000 <bocheng0000@gmail.com>
2019-01-10 11:24:52 +0800

    change HeightVersion1 for test

--- version/heights/versions.go
+++ version/heights/versions.go
@@ -2,6 +2,6 @@ package heights
 
 const (
 	GenesisHeightVersion = uint32(0)
-	HeightVersion1       = uint32(88812)
+	HeightVersion1       = uint32(100)
 	HeightVersion2       = uint32(1008812) //fixme edit height later
 )
